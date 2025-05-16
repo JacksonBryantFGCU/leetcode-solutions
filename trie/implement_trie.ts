caa14f0a33e06905a642498c0509e913c383c54c
Jackson L. Bryant <jackbryant5589@gmail.com>
2025-05-15 20:39:52 -0400

    Create implement_trie.ts

---
+++ trie/implement_trie.ts
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+
+class TrieNode {
+  children: Map<string, TrieNode>;
+  isWord: boolean;
+
+  constructor() {
+    this.children = new Map();
+    this.isWord = false;
+  }
+}
+
+class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word: string): void {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode());
+      }
+      node = node.children.get(char)!;
+    }
+    node.isWord = true;
+  }
+
+  search(word: string): boolean {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children.has(char)) return false;
+      node = node.children.get(char)!;
+    }
+    return node.isWord;
+  }
+
+  startsWith(prefix: string): boolean {
+    let node = this.root;
+    for (const char of prefix) {
+      if (!node.children.has(char)) return false;
+      node = node.children.get(char)!;
+    }
+    return true;
+  }
+}
