67a2d402b0b55a67cc2c1db8497b8114725064fa
Jackson L. Bryant <jackbryant5589@gmail.com>
2025-05-19 11:38:57 -0400

    Create decode_ways.ts

---
+++ dp_strings/decode_ways.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/decode-ways/
+
+function numDecodings(s: string): number {
+  if (!s.length || s[0] === "0") return 0;
+
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+
+  for (let i = 2; i <= s.length; i++) {
+    const one = parseInt(s.slice(i - 1, i));
+    const two = parseInt(s.slice(i - 2, i));
+
+    if (one >= 1 && one <= 9) dp[i] += dp[i - 1];
+    if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
+  }
+
+  return dp[s.length];
+}
